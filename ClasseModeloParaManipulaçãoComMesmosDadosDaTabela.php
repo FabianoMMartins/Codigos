44d9f4a61ea106932caf732e6e17b8d2481ff65a
Danilo <danilo@otimaideia.com.br>
2018-01-23 09:48:20 -0200

    ClasseModeloManipularDadosEmTabelas

---
+++ ClasseModeloParaManipulaçãoComMesmosDadosDaTabela.php
@@ -0,0 +1,44 @@
+<?php
+    class Contato {
+    
+        private $id = null;
+        private $nome;
+        private $email;
+        private $telefone;
+        
+        public function __construct($nome, $emal, $telefone) {
+           $this->nome = $nome;
+           $this->emal = $email;
+           $this->telefone = $telefone;
+        }
+        
+        //GET
+        public function getId() {
+           return $this->id;
+        }
+        public function getNome() {
+           return $this->nome;
+        }
+        public function getEmal() {
+           return $this->emal;
+        }
+        public function getTelefone() {
+           return $this->telefone;
+        }
+        
+        //SET
+        public function setId($id) {
+           $this->id = $id;
+        }
+        public function setNome($nome) {
+           $this->nome = $nome;
+        }
+        public function setEmal($emal) {
+           $this->email = $email;
+        }
+        public function setTelefone($telefone) {
+           $this->telefone = $telefone;
+        }
+        
+    }
+?>
