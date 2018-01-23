4d74a889571ddaec6cf05a42dc0e11314fc0f2ea
Danilo <danilo@otimaideia.com.br>
2018-01-23 12:07:33 -0200

    Correções necessária

--- Login/usuario.php
+++ Login/usuario.php
@@ -17,8 +17,8 @@
    //Aqui estara o metodo de acesso ao banco.
    $nome  = $this->nome;
    $senha = $this->senha;
-   $query = $query = "SELECT nome, senha FROM usuarios WHERE nome='".$nome."' AND senha='".$senha."'";
-   $resultados = mysql_query($query) or die(mysql_error();
+   $query = "SELECT nome, senha FROM usuarios WHERE nome='".$nome."' AND senha='".$senha."'";
+   $resultados = mysql_query($query) or die(mysql_error());
    if (mysql_num_rows($resultados)>0) {
     $this->geraSessao($this);
     return true;
